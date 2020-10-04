965a24f2786099dabe1cc336755be1ea68c4aa54
Ilia <ahumyck@github.com>
2020-10-04 23:50:22 +0400

    'm'

--- Object recognition methods/labs/lab1/estimation.m
+++ Object recognition methods/labs/lab1/estimation.m
@@ -9,7 +9,7 @@ B = zeros(n, n);
 
 for i = 1:n
     for j = 1:n
-        B(i, j) = (X(i, :) * (X(j, :)')) / N - M(i) * (M(j)');        
+        B(i, j) = (X(i, :) * (X(j, :)')) / N - M(i) * (M(j)');       
     end
 end
 
