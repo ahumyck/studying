66f829f39c162d5577abf78624e2a51b02520172
Ilia <ahumyck@github.com>
2020-11-30 18:13:45 +0400

    steg l3

--- Steganography/labs/l3/resources/watermark.py
+++ Steganography/labs/l3/resources/watermark.py
@@ -7,12 +7,17 @@ Created on Mon Nov 30 16:50:02 2020
 
 import os
 
+original_image = "peppers.pgm"
+embedded_image = "embed.pgm"
+
 
 def embed(input_filename, output_filename, embed_type):
     sig_name = embed_type + ".sig"
-    execute = "gen_" + embed_type + "_sig -o " + sig_name
+    execute = "gen_" + embed_type + "_sig.exe -o " + sig_name
     print('executing:', execute)
     os.system(execute)
     execute = "wm_" + embed_type + "_e.exe -s " + sig_name + " -o " + output_filename + " " + input_filename
     print('executing:', execute)
     os.system(execute)
+    
+embed(original_image, embedded_image, 'wang')
